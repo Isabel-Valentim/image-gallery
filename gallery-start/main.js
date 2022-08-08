3e313f840720343b637c0b98ac7bc74eb9b7e2fc
Isabel <isabelvalentim18@gmail.com>
2022-08-08 19:12:51 -0300

    js done

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -5,25 +5,27 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 // array all images
-const images= ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg']
 
 // add images to the thumb bar
-images.forEach(e => {
-    const imagesEle = document.createElement('img')
-    imagesEle.setAttribute('src', `images/${e}`)
-    thumbBar.appendChild(imagesEle)
-    imagesEle.classList.add('imgs')
-});
+for(const image of images) {
 
-const allImages = document.querySelectorAll('.imgs')
-//click on images
-console.log(allImages)
-function pickImage(el){
-    if(el)
-    el.target.classList.add('displayed-img')
+    const imagesEle = document.createElement('img');
+
+    imagesEle.setAttribute('src', `images/${image}`);
+
+    thumbBar.appendChild(imagesEle);
+
+    imagesEle.classList.add('imgs');
+
+    imagesEle.addEventListener('click', ele =>{
+
+        displayedImage.src = ele.target.src;
+
+        console.log(displayedImage.src)
+        
+    });
 }
-
-allImages.addEventListener('click', pickImage)
 
 
 /* Wiring up the Darken/Lighten button */
